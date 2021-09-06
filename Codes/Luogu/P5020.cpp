2ec1b5103b000da3902455e113e91299d142225d
Cherry-Areka <arekaldi@gmail.com>
2021-09-06 10:10:25 +0800

    Cpp

--- Codes/Luogu/P5020.cpp
+++ Codes/Luogu/P5020.cpp
@@ -10,10 +10,33 @@ inline int read() {
     return x * f;
 }
 
+const int maxn = 3e4 + 1;
+
+int n, a[maxn];
+
+bool f[maxn];
+
 signed main() {
-    
-    
-    
+    int T = read();
+    while(T--) {
+        n = read();
+        memset(f, false, sizeof(f));
+        int m = n;
+        for(int i = 1; i <= n; ++i)
+            a[i] = read();
+        sort(a + 1, a + n + 1);
+        // 因为 B <= A
+        f[0] = 1;
+        for(int i = 1; i <= n; ++i) {
+            if(f[a[i]] != 0)
+                // 如果 a[i] 能被表示出
+                m--;
+            else
+                for(int j = a[i]; j <= a[n]; ++j)
+                    f[j] = f[j] | f[j - a[i]];
+        }
+        printf("%d\n", m);
+    }
     
     #ifndef ONLINE_JUDGE
         getchar();
